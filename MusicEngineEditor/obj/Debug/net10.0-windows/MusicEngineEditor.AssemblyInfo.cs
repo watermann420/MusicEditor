6a2f4fd49ca78e2e8a0ae35fb02d782f1341d4c7
nullonebinary <bytezer0ffensive@protonmail.com>
2026-01-25 17:23:01 +0100

    --

--- MusicEngineEditor/obj/Debug/net10.0-windows/MusicEngineEditor.AssemblyInfo.cs
+++ MusicEngineEditor/obj/Debug/net10.0-windows/MusicEngineEditor.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2025 MusicEngine")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Professional music production and audio scripting environment")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecfe4fc70db7cb8967fd8f12acc9bfb089b6e981")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bb5c3bf215930f0002328ed5f582cb8a43f34d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngineEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
